43d5d04f455d83c5244414c7bfdd2d37c5b99300
Falk Hoppe <falkhoppe81@gmail.com>
2020-11-18 12:03:11 +0100

    initialize empty js project

--- bin/frontendtechnologi.es.js
+++ bin/frontendtechnologi.es.js
@@ -1,11 +1,1 @@
 #!/usr/bin/env node
-
-import app from '../index.js'
-
-const config = {
-  port: process.env.PORT
-}
-
-const server = app(config).listen(config.port, () => {
-  console.log(`listening to port: ${server.address().port}`)
-})
